204c3c10b9dc4b179ffd7e12c002183ce2096e73
AuroBreeze <1732373074@qq.com>
2025-04-16 22:38:41 +0800

    refactor(test): 更新注册测试数据并调整接口调用
    - 修改邮箱地址和用户名
    - 添加验证码和用途字段
    - 取消注释注册请求并注释其他请求

--- test.py
+++ test.py
@@ -16,11 +16,13 @@ login_data1={
     "password": "123123123"
 }
 register_data = {
-    "email": "123123@qq.com",
+    "email": "13122313@qq.com",
     "password": "123123123",
-    "username": "AuroBreeze111"
+    "username": "AuroBree110",
+    "code": "495261",#572741
+    "usage": "Register"
 }
-#res = requests.post(url_register, json=register_data).json()
+res = requests.post(url_register, json=register_data).json()
 #res = requests.post(url_login, json=login_data1).json()
-res = requests.post(url_codesend, json={"email": "1231231231@qq.com"}).json()
+#res = requests.post(url_codesend, json={"email": "13122313@qq.com", "usage": "Register"}).json()
 print(res)
