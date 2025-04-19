75feda8c551da4d16c638d1001fc43ba43863852
AuroBreeze <1732373074@qq.com>
2025-04-19 15:55:50 +0800

    style(user): 格式化 urls.py 文件
    - 调整了缩进，使代码更加一致和可读
    - 移除了多余的空行，精简了代码结构

--- apps/user/api/urls.py
+++ apps/user/api/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
 from .views import *
 
-#指定应用名称(命名空间)
-app_name='user'
+# 指定应用名称(命名空间)
+app_name = 'user'
 
-
-urlpatterns=[
-    path("login/",LoginAPI.as_view(),name="login"),
-    path("register/",RegisterAPI.as_view(),name="register"),
-    path("querr/",Querr.as_view(),name="querr"),
-    path("article/",ArticleAPI.as_view(),name="article"),
-    path("articlelist/",ArticleListAPI.as_view(),name="articlelist"),
-    path("emailsendcode/",EmailCodeSendAPI.as_view(),name="emailsendcode"),
-    path("resetpassword/",ResetPasswordAPI.as_view(),name="restpassword")
+urlpatterns = [
+    path("login/", LoginAPI.as_view(), name="login"),
+    path("register/", RegisterAPI.as_view(), name="register"),
+    path("querr/", Querr.as_view(), name="querr"),
+    path("article/", ArticleAPI.as_view(), name="article"),
+    path("articlelist/", ArticleListAPI.as_view(), name="articlelist"),
+    path("emailsendcode/", EmailCodeSendAPI.as_view(), name="emailsendcode"),
+    path("resetpassword/", ResetPasswordAPI.as_view(), name="restpassword")
 ]
