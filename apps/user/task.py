0b7f9b31adc516727026a5551c5b2ca498bde926
AuroBreeze <1732373074@qq.com>
2025-04-19 08:39:26 +0800

    feat(user): 优化注册邮件模板
    - 添加精美的 HTML邮件模板，提升用户体验
    - 更新邮件内容，增加欢迎语、验证码说明等信息
    - 修改邮件标题为 "欢迎注册EasyQFLP"
    - 更新发件人邮箱地址

--- apps/user/task.py
+++ apps/user/task.py
@@ -2,11 +2,32 @@ from celery import shared_task
 from django.core.mail import send_mail
 
 @shared_task
-def send_email_task(user_email,message:str):
+def send_email_task(user_email,code:str):
+    message_html = f"""
+        <html>
+            <body>
+                <h1>欢迎注册 EasyQFLP 🚀</h1>
+                <span>
+                <p>感谢您加入 EasyQFLP，这是一个基于项目的知识分享型网站，致力于打破信息差，提供更多的其他领域的<strong>从零开始</strong>的学习项目，让更多的人可以接触到更多的知识，以便寻找到属于自己的方向。🌍</p>
+                <p>您的验证码为：<strong>{code}</strong></p>
+                <p>验证码将在 5 分钟后过期。</p>
+                <p>请使用此验证码完成注册流程。</p>
+                <p>期待您的加入，一起探索知识的海洋！</p>
+                </span>
+                
+                <p>如果本操作不是您本人所发起的，请忽略此邮件。</p>
+                <a href="https://easyqflp.top/">重置密码</a>
+                
+                <p>此邮件为系统自动发送，请勿回复。</p>
+                <p>Best regards,<br>EasyQFLP Team</p>
+            </body>
+        </html>
+        """
     send_mail(
-        subject="欢迎注册",
-        message=message,
-        from_email="noreply@qq.com",
+        subject="欢迎注册EasyQFLP",
+        message=None, # 纯文本邮件
+        html_message=message_html,
+        from_email="easyqflp@nvc0qfnu.top",
         recipient_list=[user_email],
         fail_silently=False,
     )
