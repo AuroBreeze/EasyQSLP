66318492f36d456cb9b8ced520016df683010a78
AuroBreeze <1732373074@qq.com>
2025-04-19 09:03:56 +0800

    refactor(config): 重新配置开发环境设置
    - 移除 base.py 中的 CORS_ALLOW_METHODS配置
    - 在 dev.py 中添加 CORS_ALLOW_METHODS 配置
    - 更新 dev.py 中的数据库配置示例
    - 在 dev.py 中添加安装的应用配置
    -调整 dev.py 中的 ALLOWED_HOSTS 配置
    - 在 dev.py 中添加登录配置和邮箱配置相关注释

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -2,6 +2,14 @@ from .base import *
 
 DEBUG = True
 
+# 允许的请求方法
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'OPTIONS',  # 必须包含OPTIONS
+]
+
+# 数据库配置
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
@@ -18,6 +26,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+# 安装的应用
 INSTALLED_APPS_DEV = [
     'corsheaders',
     'apps.user.apps.UserConfig'
@@ -25,6 +35,7 @@ INSTALLED_APPS_DEV = [
 
 INSTALLED_APPS = INSTALLED_APPS_DEV + INSTALLED_APPS
 
+# 静态文件配置
 MEDIA_ROOT = BASE_DIR.parent / 'media'  # 媒体文件存储路径
 
 # 禁用安全警告（仅开发环境！）
@@ -32,10 +43,9 @@ SECURE_HSTS_SECONDS = 0  # 禁用HSTS
 SECURE_SSL_REDIRECT = False  # 不强制HTTPS
 SESSION_COOKIE_SECURE = False  # 允许HTTP传输Cookie
 CSRF_COOKIE_SECURE = False  # 允许HTTP传输CSRF Token
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','35a71b61.r26.cpolar.top']  # 开发环境允许的域名
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']  # 开发环境允许的域名
 
-
-
+# 登录配置
 AUTH_USER_MODEL = 'user.User_Login'  # 替换 your_app 为实际应用名
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',  # 默认后端（支持 USERNAME_FIELD）
@@ -49,6 +59,8 @@ CELERY_ACCEPT_CONTENT = ['json']#指定接受的内容类型
 CELERY_TASK_SERIALIZER = 'json'#任务序列化和反序列化方案
 CELERY_TIMEZONE = 'Asia/Shanghai'#时区
 
+
+# 邮箱配置
 import toml
 
 # 读取配置文件
