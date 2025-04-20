1a3ecd85c406fcdd225425707ff297f5694b5925
AuroBreeze <1732373074@qq.com>
2025-04-20 20:41:23 +0800

    build(dev): 修改开发环境端口和允许的主机
    - 将开发环境的端口从 63342 改为 7856
    - 更新 ALLOWED_HOSTS 和 CORS_ORIGIN_WHITELIST 以匹配新的端口
    - 修改 Vite 配置中的服务器端口为 7856

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -55,10 +55,10 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # 跨域配置
-ALLOWED_HOSTS = ['localhost','localhost:63342', '127.0.0.1:63342']  # 开发环境允许的域名
+ALLOWED_HOSTS = ['localhost','localhost:7856', '127.0.0.1:7856']  # 开发环境允许的域名
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:63342',
-    'http://127.0.0.1:63342',
+    'http://localhost:7856',
+    'http://127.0.0.1:7856',
 )  # 允许跨域请求的域名
 
 #celery配置
