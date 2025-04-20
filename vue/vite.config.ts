b2cb3e630425e3d2421bff2fbeee935c0f5aaf91
AuroBreeze <1732373074@qq.com>
2025-04-20 20:41:23 +0800

    build(dev): 修改开发环境端口和允许的主机
    - 将开发环境的端口从 63342 改为 7856
    - 更新 ALLOWED_HOSTS 和 CORS_ORIGIN_WHITELIST 以匹配新的端口
    - 修改 Vite 配置中的服务器端口为 7856

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 8080 // 设置开发服务器端口为3000
+    port: 7856 // 设置开发服务器端口为3000
   },
   plugins: [
     vue(),
