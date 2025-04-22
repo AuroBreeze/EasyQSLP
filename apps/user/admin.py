d58b1336fb2f8c9d8fe84dcde7b7f56a72d89dd0
AuroBreeze <1732373074@qq.com>
2025-04-22 06:38:17 +0000

    refactor(user): 更新 User_Login 模型的 admin 注册方式
    - 使用 @admin.register 装饰器注册 User_Login 模型
    - 删除了多余的 admin.site.register(User_Login, User_LoginAdmin) 行

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-
+@admin.register(User_Login)
 class User_LoginAdmin(admin.ModelAdmin):
     list_display = ('email', 'username','join_date', 'last_login','is_active', 'is_staff', 'is_superuser') # 显示字段
     list_filter = ('is_active', 'is_staff', 'is_superuser') # 过滤器
@@ -15,5 +15,3 @@ class User_LoginAdmin(admin.ModelAdmin):
     readonly_fields = ('join_date', 'last_login') # 只读字段
     list_per_page = 15 # 每页显示条数
 
-
-admin.site.register(User_Login, User_LoginAdmin)
