b466475237a85175a27df6ed6237a98589efdd91
AuroBreeze <1732373074@qq.com>
2025-04-24 16:47:24 +0800

    test(projectmanage): 添加文章测试用例并完善接口
    - 新增 ArticleTestCase 类，包含创建和读取文章的测试用例
    - 更新 urls.py，添加文章详情接口
    - 修改 views.py，实现文章详情的 GET 请求

--- apps/projectmanage/manage/views.py
+++ apps/projectmanage/manage/views.py
@@ -1,3 +1,5 @@
+from rest_framework.generics import get_object_or_404
+
 from .serializers import ArticleSerializer
 from ..models import Article
 from django.core.cache import cache
@@ -5,8 +7,15 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 class ArticleView(APIView):
-    def get(self,request):
-        articles = Article.objects.all()
+    def get(self,request,*args,**kwargs):
+        article_id = kwargs.get('pk')
+        #print(article_id)
+        article = get_object_or_404(Article,pk=article_id)
+        #print(article)
+        serializer = ArticleSerializer(article)
+        #print(serializer.data)
+        return Response(serializer.data)
+
     def post(self,requests):
         serializer = ArticleSerializer(data=requests.data)
         if serializer.is_valid():
--- apps/projectmanage/tests.py
+++ apps/projectmanage/tests.py
@@ -5,25 +5,42 @@ from rest_framework import status
 from .models import Article,Article_category
 from apps.user.models import User_Login
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # Create your tests here.
 
 class ArticleTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.article_url = reverse('project:articletest')
+        self.articlepost_url = reverse('project:articletest')
+        self.articleget_url = reverse('project:article',kwargs={'pk':1})
         self.user = User_Login.objects.create_user(email='test@example.com', password='testpassword', username='testuser')
         self.category = Article_category.objects.create(name='Test Category')
         self.valided_data = {
             'title': 'Test Article',
-            'content_md': "'''This is a test article.'''",
+            'content_md': "'''This is a test article123.'''",
+            'adminer': self.user.id,
             #'creat_time':timezone.now()
-            'id':self.user.id
+            #'id':self.user.id
         }
 
     def test_create_article(self):
-        response = self.client.post(self.article_url, self.valided_data, format='json')
-        print(response.data)
+        response = self.client.post(self.articlepost_url, self.valided_data, format='json')
+        #print(response.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Article.objects.count(), 1)
         self.assertEqual(Article.objects.get().title, 'Test Article')
+
+    def test_read_article_valid_data(self):
+        response = self.client.post(self.articlepost_url, self.valided_data, format='json')
+        #print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Article.objects.count(), 1)
+        self.assertEqual(Article.objects.get().title, 'Test Article')
+
+
+        response = self.client.get(self.articleget_url, format='json')
+        #print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
 
