f6c7d5724a8b8e7c5ede9432158b63753f6b68da
AuroBreeze <1732373074@qq.com>
2025-04-24 16:09:59 +0800

    refactor(projectmanage): 重构文章视图类
    - 移除了 ArticleViewMixin 和 ArticleDetailView 类- 新增了 ArticleView 类，支持 GET 和 POST 请求
    -简化了代码结构，提高了可维护性

--- apps/projectmanage/manage/views.py
+++ apps/projectmanage/manage/views.py
@@ -1,28 +1,16 @@
 from .serializers import ArticleSerializer
 from ..models import Article
-from rest_framework import generics
 from django.core.cache import cache
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-class ArticleViewMixin(generics.GenericAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-    def get_queryset(self):
-        return super().get_queryset().select_related('project', 'category', 'adminer')
-
-class ArticleDetailView(ArticleViewMixin,generics.RetrieveUpdateDestroyAPIView):
-    def retrieve(self, request, *args, **kwargs):
-        cache_key = f'article_{kwargs["pk"]}_html'
-        cached_html = cache.get(cache_key)
-
-        if cached_html:
-            return Response({'html': cached_html})
-
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-
-        cache.set(cache_key, serializer.data['content_html'], 60 * 60 * 12)
-        return Response(serializer.data)
+class ArticleView(APIView):
+    def get(self,request):
+        articles = Article.objects.all()
+    def post(self,requests):
+        serializer = ArticleSerializer(data=requests.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.errors,status=400)
 
