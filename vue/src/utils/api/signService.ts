43b756de9c25499f6d819aa79c35b8d8491df82c
AuroBreeze <1732373074@qq.com>
2025-04-26 21:09:02 +0800

    feat(auth): 新增登录、注册和重置密码功能
    - 实现了 Login、Register 和 ResetPassword 三个函数
    - 定义了相应的请求接口和响应数据结构
    - 使用 http 模块发送请求并处理响应
    - 函数导出以便在其他模块中使用

---
+++ vue/src/utils/api/signService.ts
@@ -0,0 +1,86 @@
+import { Interface } from 'readline'
+import request from './http'
+
+interface LoginResponse {
+    success: boolean
+    message: string
+    username?: string
+    errors?: any
+
+}
+async function Login(email: string, password: string): Promise<LoginResponse> {
+    const valid = { email, password }
+    try{
+        const response = await request.post('/api/v1/user/login/', valid)
+        return {
+            success: response.success,
+            message: response.message,
+            username: response.username,
+        }
+    }catch(error: any){
+        return {
+            success: error.success,
+            message: error.message,
+            errors: error.errors,
+        }
+    }
+}
+
+
+interface RegisterResponse {
+    success: boolean
+    message: string
+    errors?: any
+    
+}
+async function Register(email: string, password: string, username: string ,code: string): Promise<RegisterResponse> {
+    const valid = { email, password, username, code }
+    try{
+        const response = await request.post('/api/v1/user/register/', valid)
+        return {
+            success: response.success,
+            message: response.message,
+        }
+    }catch(error: any){
+        return {
+            success: error.success,
+            message: error.message,
+            errors: error.errors,
+        }
+    }
+}
+
+interface ResetPasswordResponse {
+    success: boolean
+    message: string
+    errors?: any
+}
+async function ResetPassword(email: string, code: string, password: string,password_confirm: string): Promise<ResetPasswordResponse> {
+    const valid = { email, code, password, password_confirm }
+    try{
+        const response = await request.post('/api/v1/user/reset_password/', valid)
+        return {
+            success: response.success,
+            message: response.message,
+        }
+    }catch(error: any){
+        return {
+            success: error.success,
+            message: error.message,
+            errors: error.errors,
+        }
+    }
+}
+
+export default {
+    Login(email: string, password: string){
+        return Login(email, password)
+    },
+    Register(email: string, password: string, username: string, code: string){
+        return Register(email, password, username, code)
+    },
+    ResetPassword(email: string, code: string, password: string, password_confirm: string){
+        return ResetPassword(email, code, password,password_confirm)
+    }
+}
+
