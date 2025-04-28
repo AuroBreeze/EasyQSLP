f56a406466c72b5f0730d86a124175b9b149f114
AuroBreeze <1732373074@qq.com>
2025-04-28 11:41:05 +0800

    refactor(user): 重构用户模块并更新环境配置
    - 修改用户注册和登录接口的错误处理逻辑
    - 更新环境配置文件中的邮箱设置
    - 调整 URLs 配置
    - 更新用户模块的测试用例

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include("apps.api.urls")),
-    # path('user/',include("apps.user.api.urls")), #测试专用
+    path('user/',include("apps.user.api.urls")), #测试专用
     # path('project/',include("apps.projectmanage.manage.urls")),
 
 ]
