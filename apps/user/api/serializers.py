f1b47552eff15a8a478fdbacaa089c84351f512e
AuroBreeze <1732373074@qq.com>
2025-05-31 16:20:38 +0800

    feat(user): 生成用户简介目录和字数统计
    - 修改 UserProfileSerializer 类，添加生成目录和字数统计功能
    - 更新 get_toc 和 get_word_count 方法，使用 userprofile_md 字段
    - 在 Meta 类中添加 toc 和 word_count 字段

--- apps/user/api/serializers.py
+++ apps/user/api/serializers.py
@@ -162,11 +162,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
         生成目录
         """
         md = markdown.Markdown(extensions=['markdown.extensions.toc'])
-        md.convert(obj.content_md)
+        md.convert(obj.userprofile_md)
         return md.toc
 
     def get_word_count(self, obj):
-        return len(obj.content_md.split())
+        return len(obj.userprofile_md.split())
+
     class Meta:
         model = User_Profile
-        fields = ['avatar','userprofile_md','userprofile_html','content_hash','create_time','update_time','user_Login']
+        fields = ['avatar','userprofile_md','userprofile_html','content_hash','create_time','update_time','user_Login','toc','word_count']
